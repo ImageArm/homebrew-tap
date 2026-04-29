adc0b1ca26646d5e2a933085a2fb7d82be64631a
Julien CADIOU <madjuju@users.noreply.github.com>
2026-04-29 15:23:29 +0200

    Update imagearm cask: 1.3.1 → 1.3.2

--- Casks/imagearm.rb
+++ Casks/imagearm.rb
@@ -1,6 +1,6 @@
 cask "imagearm" do
-  version "1.3.1"
-  sha256 "2a36f730124e7326ec52bebd0b7a04d0df44b011bf297bc6f11187b3b75d9c3b"
+  version "1.3.2"
+  sha256 "a5f9299c2d782d15e8e227791aaed4307de74e7839d402075ee39c7398ea8048"
 
   url "https://github.com/ImageArm/ImageArm/releases/download/v#{version}/ImageArm-#{version}.dmg"
   name "ImageArm"
